abe456e89d794a9dc99680ebf7bd7905cdcd1606
Mond Wan <mondwan.1015@gmail.com>
2015-04-15 20:51:29 +0800

    Add LobbyActivity and change the base class of HomeActivity
    --Overview
    1. Since we don't need the ActionBar, we can inherits Activity directly

--- app/src/main/java/com/iems5722/partytime/LobbyActivity.java
+++ app/src/main/java/com/iems5722/partytime/LobbyActivity.java
@@ -4,10 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 
-public class HomeActivity extends Activity {
+public class LobbyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_lobby);
     }
 }

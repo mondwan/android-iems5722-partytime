01f9efe7a785894cb3d6ae049174d01e35fa5683
Kevin Chan <kevincpy@gmail.com>
2015-04-20 01:35:31 +0800

    Added random generator Random between 10 -20

--- app/src/main/java/com/iems5722/partytime/StopwatchActivity.java
+++ app/src/main/java/com/iems5722/partytime/StopwatchActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+//import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+import java.util.Random;
 
 
 /**
@@ -19,14 +21,26 @@ import android.widget.ToggleButton;
     private static final String TAG = StopwatchActivity.class.getClass().getSimpleName();
     long init,now,time,paused;
     TextView display;
+    TextView rule;
     Handler handler;
+    int randnum;
+    int scores;
+    //boolean stopped;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
         final ToggleButton passTog = (ToggleButton) findViewById(R.id.onoff);
+        //final Button stopButton = (Button) findViewById(R.id.stop);
         display = (TextView) findViewById(R.id.display);
+        rule = (TextView) findViewById(R.id.game_rule);
         handler = new Handler();
+        randNumGen();
+        //stopped = false;
+        rule.setText("Stop the timer after " + randnum + " seconds!");
+        Log.d(TAG, "onCreate");
+
+
         final Runnable updater = new Runnable() {
             @Override
             public void run() {
@@ -58,33 +72,52 @@ import android.widget.ToggleButton;
         passTog.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
+                Log.d(TAG, "Click received in onClick.");
                 init = System.currentTimeMillis();
                 handler.post(updater);
             }
         });
 
+//        stopButton.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                showElapsedTime(time);
+//            }
+//        });
+
     }
     @Override
     protected void onPause() {
+        Log.d(TAG, "Click received in onPause.");
         super.onPause();
         paused = System.currentTimeMillis();
     }
 
     @Override
     protected void onResume() {
+        Log.d(TAG, "Click received in onResume.");
         super.onResume();
         init += System.currentTimeMillis() - paused;
     }
     private void showElapsedTime(Long stoptime) {
-        //long elapsedMillis = SystemClock.elapsedRealtime() - mChronometer.getBase();
         Integer mstoptime = (int) (long) stoptime;
         Log.d(TAG, "stoppedMilliseconds: " + mstoptime);
-        int offset = 10000 - Math.abs(10 * 1000 - mstoptime);
+        int offset = randnum * 1000 - Math.abs(randnum * 1000 - mstoptime);
         Log.d(TAG, "offset: " + offset);
-        int scores = Math.round(offset / 10);
-
+        scores = Math.round(offset / randnum);
+        Log.d(TAG, "scores: " + scores);
         Toast.makeText(StopwatchActivity.this, "Your scores is " + scores,
                 Toast.LENGTH_SHORT).show();
 
     }
+
+    private void randNumGen(){
+
+        int max = 20;
+        int min = 10;
+
+        Random random = new Random();
+        randnum = random.nextInt(max - min + 1) + min;
+
+    }
+
 }

606552760897bb3e347740722a8ddde8fd59e5b7
Kevin Chan <kevincpy@gmail.com>
2015-04-22 00:49:08 +0800

    added scores update to CrazyClickActivity

--- app/src/main/java/com/iems5722/partytime/CrazyClickActivity.java
+++ app/src/main/java/com/iems5722/partytime/CrazyClickActivity.java
@@ -3,13 +3,14 @@ package com.iems5722.partytime;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
 public class CrazyClickActivity extends PortraitOnlyActivity {
-
+    private static final String TAG = CrazyClickActivity.class.getClass().getSimpleName();
     final int gameTime = 10;
 
     Button crazyButton;
@@ -18,7 +19,7 @@ public class CrazyClickActivity extends PortraitOnlyActivity {
 
     Boolean isFinish = false;
     int score = 0;
-
+    protected GameController gameController = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +29,33 @@ public class CrazyClickActivity extends PortraitOnlyActivity {
         counterView.setText("0");
         timeView = (TextView) this.findViewById(R.id.timeView);
         crazyButton = (Button) this.findViewById(R.id.crazyButton);
+
+        this.gameController = GameController.getInstance();
+        final GameController.UpdateScoresRequest req = new GameController.UpdateScoresRequest();
+        final GameController.UpdateScoresResponse res = new GameController.UpdateScoresResponse();
         crazyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isFinish) {
                     score++;
                     counterView.setText(Integer.toString(score));
+                    if (gameController.isHost()){
+                        res.scores = score;
+                        res.requestIP = gameController.localIP;
+                        res.serverIP = gameController.getServerIP();
+                        gameController.sendMsg(res);
+                        gameController.setGamePlayerScores(res.serverIP,res.scores);
+                        GamePlayer player = gameController.getGamePlayer(res.serverIP);
+                        Log.d(TAG, String.format("Scores Update(Server self): Player |%s|, Scores|%s|", player.getUsername(), player.getScores()));
+                    }
+                    else{
+                        req.scores = score;
+                        req.requestIP = gameController.localIP;
+                        gameController.sendMsg(req);
+                        gameController.setGamePlayerScores(req.requestIP,req.scores);
+                        GamePlayer player = gameController.getGamePlayer(req.requestIP);
+                        Log.d(TAG, String.format("Scores Update(Client self): Player |%s|, Scores|%s|", player.getUsername(), player.getScores()));
+                    }
                 }
             }
         });
--- app/src/main/java/com/iems5722/partytime/LobbyActivity.java
+++ app/src/main/java/com/iems5722/partytime/LobbyActivity.java
@@ -106,8 +106,8 @@ public class LobbyActivity extends PortraitOnlyActivity {
         this.startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LobbyActivity.this, SendScoresActivity.class);
-                Log.d(TAG, "SendScores button clicked");
+                Intent intent = new Intent(LobbyActivity.this, CrazyClickActivity.class);
+                Log.d(TAG, "CrazyClick button clicked");
                 startActivity(intent);
             }
         });
